9e0ec48598e1f5cbdee42ca8170c01da32346ac8
Varun Dutia <varundutia.h@gmail.com>
2021-04-27 14:28:31 +0530

    location service implemented

--- client_app/app/src/main/java/com/example/client_containment/MainActivity.java
+++ client_app/app/src/main/java/com/example/client_containment/MainActivity.java
@@ -1,15 +1,122 @@
 package com.example.client_containment;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
+import android.Manifest;
+import android.app.ActivityManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.client_containment.Service.Constants;
+import com.example.client_containment.Service.LocationService;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            // Get extra data included in the Intent
+            String message = intent.getStringExtra("Status");
+            Bundle b = intent.getBundleExtra("Location");
+            LocationResult lastKnownLoc = (LocationResult) b.getParcelable("Location");
+            String lat = String.valueOf(lastKnownLoc.getLastLocation().getLatitude());
+            String lon = String.valueOf(lastKnownLoc.getLastLocation().getLongitude());
+            String loc = lat + " "+ lon;
+            if (lastKnownLoc != null) {
+                Toast.makeText(context,loc,Toast.LENGTH_SHORT).show();
+            }
+//             Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LocalBroadcastManager.getInstance(this).registerReceiver(
+                mMessageReceiver, new IntentFilter("LocationUpdate"));
+        findViewById(R.id.button_start_location).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(ContextCompat.checkSelfPermission(getApplicationContext(),
+                        Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(
+                            MainActivity.this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_LOCATION_PERMISSION
+                    );
+                }else{
+                    startLocationService();
+                }
+            }
+        });
+        findViewById(R.id.button_stop_location).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stopLocationService();
+            }
+        });
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_LOCATION_PERMISSION && grantResults.length > 0){
+            if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                startLocationService();
+            } else {
+                Toast.makeText(this,"NO permisson",Toast.LENGTH_LONG)
+                        .show();
+            }
+        }
+    }
+
+    private boolean isLocationServiceRunning() {
+        ActivityManager activityManager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        if(activityManager != null){
+            for(ActivityManager.RunningServiceInfo service :
+            activityManager.getRunningServices(Integer.MAX_VALUE)){
+                if(LocationService.class.getName().equals(service.service.getClassName())){
+                    if(service.foreground){
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+        return false;
+    }
+    private void startLocationService() {
+        if(!isLocationServiceRunning()){
+            Intent intent = new Intent(getApplicationContext(),LocationService.class);
+            intent.setAction(Constants.ACTION_START_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this,"Location Service Started",Toast.LENGTH_LONG)
+                    .show();
+        }
+    }
+
+    private void stopLocationService() {
+        if(!isLocationServiceRunning()){
+            Intent intent = new Intent(getApplicationContext(),LocationService.class);
+            intent.setAction(Constants.ACTION_STOP_LOCATION_SERVICE);
+            startService(intent);
+            Toast.makeText(this,"Location Service Stopped",Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 }
--- client_app/app/src/main/java/com/example/client_containment/Service/Constants.java
+++ client_app/app/src/main/java/com/example/client_containment/Service/Constants.java
@@ -1,4 +1,7 @@
 package com.example.client_containment.Service;
 
 public class Constants {
+    public static final int LOCATION_SERVICE_ID = 175;
+    public static final String ACTION_START_LOCATION_SERVICE = "startLocationService";
+    public static final String ACTION_STOP_LOCATION_SERVICE = "stopLocationService";
 }
